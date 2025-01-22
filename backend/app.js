06ae20e3e16095d2a07d180592d2dcf7d7dee65b
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-01-22 22:16:46 +0530

    backend routes changed

--- backend/app.js
+++ backend/app.js
@@ -16,32 +16,17 @@ app.use(
 );
 
 app.use(
-  cors()
-  //   {
-  //   // origin: process.env.FRONTEND_URL,
-  //   origin: ["https://assignment-whitecarrot-intern-2025-beta.vercel.app"],
-  //   methods: ["GET", "POST", "PUT", "DELETE"],
-  //   credentials: true,
-  // }
+  cors({
+    origin: process.env.FRONTEND_URL,
+    // origin: ["https://assignment-whitecarrot-intern-2025-beta.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
 
-// console.log(process.env.FRONTEND_URL);
+console.log(process.env.FRONTEND_URL);
 
 app.use(authRoutes);
-
-app.get("/events", async (req, res) => {
-  try {
-    if (!req.session.tokens) {
-      return res.status(401).send("Unauthorized");
-    }
-
-    const events = await getCalendarEvents(req.session.tokens);
-    res.json(events);
-  } catch (error) {
-    console.error("Error fetching calendar events:", error);
-    res.status(500).send("Error fetching calendar events");
-  }
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port: ${process.env.PORT}`);

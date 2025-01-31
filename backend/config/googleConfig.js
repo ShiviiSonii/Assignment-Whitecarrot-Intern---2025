3d7d6018526ef86cb6ca9e25f2568aa2d1cc86a5
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-01-31 22:36:24 +0530

    uri changed

--- backend/config/googleConfig.js
+++ backend/config/googleConfig.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const oAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URL
+  "https://assignment-whitecarrot-intern-2025-bjwz.vercel.app/auth/google/callback"
 );
 
 export const SCOPES = ["https://www.googleapis.com/auth/calendar"];
